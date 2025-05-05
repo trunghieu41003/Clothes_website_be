0ac3b7ebe366410c35e4b795a0d6681174aa47af
trunghieu41003 <146319700+trunghieu41003@users.noreply.github.com>
2025-05-05 20:12:39 +0700

    fitness lastest

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-// config/database.js
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('fitnessdb', 'root', 'Trunghieu2003', {
-  host: '127.0.0.1',  // hoặc 'localhost'
-  dialect: 'mysql',   // có thể là 'mysql', 'postgres', 'sqlite', hoặc 'mssql'
-});
-
-module.exports = sequelize;
